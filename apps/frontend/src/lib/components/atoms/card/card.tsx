796a069bf9fc9bac8cd3cb9bae1f651fb803e4ee
Bluzzi <camilledugas.pro@gmail.com>
2023-04-13 13:01:32 +0200

    style(card.tsx): change background color class from bg-primary-2 to bg-primary feat(card): add card component to atoms folder

--- apps/frontend/src/lib/components/atoms/card/card.tsx
+++ apps/frontend/src/lib/components/atoms/card/card.tsx
@@ -4,7 +4,7 @@ import { clsx } from "clsx";
 
 export function Card({ children, className, ...props }: CardProps): ReactElement {
   return (
-    <div className={clsx("bg-primary-2 py-4 px-5 shadow rounded", className)} {...props}>
+    <div className={clsx("bg-primary py-4 px-5 shadow rounded", className)} {...props}>
       {children}
     </div>
   );
