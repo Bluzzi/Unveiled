af62b4cba3c487eb78b22cfc7ee37355c2059af3
Bluzzi <camilledugas.pro@gmail.com>
2023-04-13 14:12:23 +0200

    style(link-button.tsx): update LinkButton styles to match design specifications

--- apps/frontend/src/lib/components/atoms/button/link-button.tsx
+++ apps/frontend/src/lib/components/atoms/button/link-button.tsx
@@ -5,14 +5,12 @@ import Link from "next/link";
 
 export function LinkButton({ size = "base", target = "_self", icon, children, className, ...props }: LinkButtonProps): ReactElement {
   const styles = clsx(
-    "rounded shadow",
-    "border-2 border-transparent",
+    "bg-primary-2 text-white text-base",
+    "rounded hover:shadow-md shadow-primary-2",
     "flex items-center justify-center gap-2",
-    "text-base",
-    "bg-primary text-white hover:brightness-125",
     {
-      "py-1 px-3": size === "small",
-      "py-2 px-5": size === "base" || size === "full-width",
+      "py-2 px-3": size === "small",
+      "py-3 px-5": size === "base" || size === "full-width",
       "w-full": size === "full-width"
     },
     className
