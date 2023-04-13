aec04b06ba05ee2699476ee4c9f672073af613e0
Bluzzi <camilledugas.pro@gmail.com>
2023-04-13 12:05:32 +0200

    feat(layout.tsx): update metadata title and description to better reflect the game's purpose and mechanics

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -6,8 +6,11 @@ import type { Metadata } from "next";
 import { Provider } from "./provider";
 
 export const metadata: Metadata = {
-  title: "Unveiled",
-  description: "A NextJS template by Leepos.",
+  title: "Unveiled - Discover who's the liar",
+  description: [
+    "Unveiled is a captivating word game, do not fall of the traps and strategy of the players,",
+    "discover who is the imposter in an interactive turn game."
+  ].join(", "),
   icons: "/logo.png"
 };
 
