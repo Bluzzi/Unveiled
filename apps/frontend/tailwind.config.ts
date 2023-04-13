58592854129fac70e9b3ba10252502749fb078da
Bluzzi <camilledugas.pro@gmail.com>
2023-04-13 13:14:26 +0200

    feat(tailwind.config.ts): add new md size to boxShadow utility class

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -46,7 +46,8 @@ export default {
     },
 
     boxShadow: {
-      DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)"
+      DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+      md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
     },
 
     extend: {
