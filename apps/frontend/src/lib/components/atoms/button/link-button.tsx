73bcf70bedbbb5b7c7578582ff547f183ed74742
Bluzzi <camilledugas.pro@gmail.com>
2023-04-13 13:42:03 +0200

    feat(button): add support for full-width size feat(button): change button styles to match design feat(button): change button icon position to be before text feat(link-button): change link button styles to match design feat(link-button): change link button icon position to be before text

--- apps/frontend/src/lib/components/atoms/button/link-button.tsx
+++ apps/frontend/src/lib/components/atoms/button/link-button.tsx
@@ -3,23 +3,20 @@ import { LinkButtonProps } from "./button.type";
 import { clsx } from "clsx";
 import Link from "next/link";
 
-export function LinkButton({ size = "base", target = "_self", fullWidth, icon, children, className, ...props }: LinkButtonProps): ReactElement {
+export function LinkButton({ size = "base", target = "_self", icon, children, className, ...props }: LinkButtonProps): ReactElement {
   const styles = clsx(
     "rounded shadow",
     "border-2 border-transparent",
     "flex items-center justify-center gap-2",
-    "bg-primary text-white",
-    "hover:brightness-110 active:scale-95 transition-transform",
+    "text-base",
+    "bg-primary text-white hover:brightness-125",
     {
-      "py-1 px-3 text-base": size === "small",
-      "py-2 px-5 text-base": size === "base",
-      "py-3 px-8 text-lg": size === "large",
-
-      "w-full": fullWidth,
-      "w-fit": !fullWidth
+      "py-1 px-3": size === "small",
+      "py-2 px-5": size === "base" || size === "full-width",
+      "w-full": size === "full-width"
     },
     className
   );
 
-  return <Link className={styles} target={target} {...props}>{children} {icon}</Link>;
+  return <Link className={styles} target={target} {...props}>{icon} {children}</Link>;
 }
